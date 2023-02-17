e6148a4062d2b4b10d5933954629dd9c79106d92
zainHome <zainafzal0100@gmail.com>
2023-02-17 09:44:14 +0530

    initial commit to add forEach polyfill

---
+++ array_method_polyfills/ForEach/forEach.js
@@ -0,0 +1,18 @@
+/*
+# what forEach does ....
+# it itereate throght all the array elements and run given callback function for every element of array,
+# forEach returns undefined.
+*/
+Array.prototype.myFor=function(cb_fn){
+    // here this will representing the myFor method's parent
+    for(var i=0;i<this.length;i++){
+        cb_fn(this[i])
+    }
+}
+
+let names=['Afzal','Yasir','Shahzad',undefined,undefined,null,NaN,2,3.5,true,{},[]];
+function cb(value){
+    console.log(value)
+}
+// givent cb function will run for every element of names array
+names.myFor(cb)
